838f49f854a30938363f82ee73633d94ca171ea8
Usman Muneer <129029349+UsmanMuneer15@users.noreply.github.com>
2023-05-07 04:49:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+// import '@fortawsome/fontawsome-free/css/all.css'
+// import '@fortawsome/fontawsome-free/js/all.css'
+
+createApp(App).mount('#app')
